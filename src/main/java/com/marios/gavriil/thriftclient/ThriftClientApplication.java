ed51773ebd74ee099fdc3007af5a741afe617ce2
Marios <mikopos@gmail.com>
2019-10-06 22:29:51 +0300

    removed unused imports

--- src/main/java/com/marios/gavriil/thriftclient/ThriftClientApplication.java
+++ src/main/java/com/marios/gavriil/thriftclient/ThriftClientApplication.java
@@ -1,10 +1,7 @@
 package com.marios.gavriil.thriftclient;
 
-import com.marios.gavriil.thriftclient.services.LogsService;
-import com.marios.gavriil.thriftclient.services.impl.LogsServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
